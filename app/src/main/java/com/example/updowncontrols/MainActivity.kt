3791c478ef5460b7733c8fcb619efaa943ae84c4
Joao Vitor <jvsouza194@gmail.com>
2025-03-13 20:13:59 -0300

    Second commit.

--- app/src/main/java/com/example/updowncontrols/MainActivity.kt
+++ app/src/main/java/com/example/updowncontrols/MainActivity.kt
@@ -3,18 +3,37 @@ package com.example.updowncontrols
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var contador = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Referências aos componentes da interface
+        val txtContador = findViewById<TextView>(R.id.activity_main_txt_contador)
+        val btnAumentar = findViewById<Button>(R.id.activity_main_btn_aumentar)
+        val btnDiminuir = findViewById<Button>(R.id.activity_main_btn_diminuir)
+
+        // Atualiza o TextView com o valor inicial
+        txtContador.text = contador.toString()
+
+        // Aumentar contador
+        btnAumentar.setOnClickListener {
+            contador++
+            txtContador.text = contador.toString()
         }
+
+        // Diminuir contador
+        btnDiminuir.setOnClickListener {
+            contador--
+            txtContador.text = contador.toString()
+        }
+
     }
 }
